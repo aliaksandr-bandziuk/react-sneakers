2d311d9d5342fdd06d3f2f5fcfb6838976ad154b
Aliaksandr Bandziuk <aliaksandr.bandziuk@gmail.com>
2022-10-07 17:08:42 +0300

    feat: create base layout

--- src/App.js
+++ src/App.js
@@ -1,12 +1,44 @@
 function App() {
   return (
     <div className="wrapper">
+      <div style={{display: 'none'}} className="overlay">
+        <div className="drawer">
+          <h2 className="mb-30 d-flex justify-between align-center">
+            Basket<img className="removeBtn" src="/img/btn-remove.svg" alt="Remove" />
+          </h2>
+          <div className="items">
+            <div className="cartItem d-flex align-center mb-20">
+              <img className="mr-15" width={70} height={70} src="/img/sneakers/1.jpg" alt="" />
+              <div className="mr-20">
+                <p className="mb-10">Мужские Кроссовки Nike Air Max 270</p>
+                <p><b>12 999 руб.</b></p>
+              </div>
+              <img className="removeBtn" src="/img/btn-remove.svg" alt="Remove" />
+            </div>
+          </div>
+          <div className="cartTotalBlock">
+            <ul>
+              <li>
+                <span>Итого:</span>
+                <div></div>
+                <b>21 498 руб.</b>
+              </li>
+              <li>
+                <span>Налог 5%:</span>
+                <div></div>
+                <b>1074 руб.</b>
+              </li>
+            </ul>
+            <button className="greenButton">Confirm <img src="/img/arrow.svg" alt="" /></button>
+          </div>
+        </div>
+      </div>
       <header className="d-flex justify-between align-center p-40">
         <div className="d-flex align-center">
           <img width={40} height={40} src="/img/logo.png" alt="" />
           <div className="headerInfo">
             <h3 className="text-uppercase">React Sneakers</h3>
-            <p className="opacity-5">Магазин лучших кроссовок</p>
+            <p className="opacity-5">Sport online shop</p>
           </div>
         </div>
         <ul className="d-flex">
@@ -19,9 +51,18 @@ function App() {
         </ul>
       </header>
       <div className="content p-40">
-        <h1 className="mb-30">Все кроссовки</h1>
+        <div className="d-flex justify-between align-center mb-30">
+          <h1>Все кроссовки</h1>
+          <div className="search-block d-flex">
+            <img src="/img/search.svg" alt="search" />
+            <input placeholder="Поиск..." type="text" name="" id="" />
+          </div>
+        </div>
         <div className="d-flex flex-wrap cards">
           <div className="card">
+            <div className="favorite">
+              <img src="/img/heart-unliked.svg" alt="button unliked" />
+            </div>
             <img width={133} height={112} src="/img/sneakers/1.jpg" alt="" />
             <p>Мужские Кроссовки Nike Blazer Mid Suede</p>
             <div className="d-flex justify-between align-center">
